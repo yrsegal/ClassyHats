962a402ddc3850f1f1e297c59f089ac64a9e4891
yrsegal <yrsegal@gmail.com>
2017-08-31 22:06:35 -0400

    blacklistu

--- src/main/java/wiresegal/classy/hats/common/core/HatConfigHandler.kt
+++ src/main/java/wiresegal/classy/hats/common/core/HatConfigHandler.kt
@@ -1,5 +1,6 @@
 package wiresegal.classy.hats.common.core
 
+import com.google.gson.JsonArray
 import com.google.gson.JsonParser
 import com.teamwizardry.librarianlib.features.kotlin.json
 import com.teamwizardry.librarianlib.features.kotlin.serialize
@@ -14,10 +15,10 @@ import java.io.File
 object HatConfigHandler {
 
     val missingno = Hat("missingno", 0.0, true)
-    var hats = mutableMapOf(
+    val hats = mutableMapOf(
+            "missingno" to missingno,
             "tophat" to Hat("tophat")
     )
-        private set
 
     var rpl = File("")
         private set
@@ -26,8 +27,8 @@ object HatConfigHandler {
         fun toJson() = json {
             obj(
                     "name" to name,
-                    "weight" to weight,
-                    "elusive" to elusive
+                    *(if (weight != 1.0) arrayOf("weight" to weight) else arrayOf()),
+                    *(if (elusive) arrayOf("elusive" to elusive) else arrayOf())
             )
         }
     }
@@ -39,7 +40,11 @@ object HatConfigHandler {
             try {
                 val newHats = mutableMapOf<String, Hat>()
                 val load = JsonParser().parse(path.reader()).asJsonObject
-                val hats = load.getAsJsonArray("hats")
+
+                val blacklist = if (load.has("blacklist_hats")) load.getAsJsonArray("blacklist_hats") else JsonArray()
+                blacklist.map { it.asString }.filter { it != "missingno" }.forEach { hats.remove(it) }
+                
+                val hats = if (load.has("custom_hats")) load.getAsJsonArray("custom_hats") else JsonArray()
                 for (hat in hats) {
                     val hatObj = hat.asJsonObject
                     val name = hatObj.getAsJsonPrimitive("name").asString
@@ -47,7 +52,7 @@ object HatConfigHandler {
                     val elusive = if (hatObj.has("elusive")) hatObj.getAsJsonPrimitive("elusive").asBoolean else false
                     newHats.put(name, Hat(name, weight, elusive))
                 }
-                this.hats = newHats
+                this.hats.putAll(newHats)
                 succeeded = true
             } catch (e: Exception) {
                 ClassyHats.LOGGER.error("Failed to parse hat config $path")
@@ -57,8 +62,10 @@ object HatConfigHandler {
         if (!succeeded) {
             path.writeText(json {
                 obj(
-                        "__comment" to "The folder classyhats_resources will act as a resource pack for any custom hats you want to add.",
-                        "hats" to array(*hats.values.map { it.toJson() }.toTypedArray())
+                        "__comment0" to "The folder classyhats_resources will act as a resource pack for any custom hats you want to add.",
+                        "__comment1" to "Put any hat names from the default hats you don't want in the blacklist.",
+                        "blacklist_hats" to array(),
+                        "custom_hats" to array(Hat("example", 2.0, true).toJson())
                 )
             }.serialize())
         }
