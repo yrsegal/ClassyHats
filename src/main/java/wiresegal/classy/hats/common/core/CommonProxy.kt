12c7c23ae2ef10205665d56f0ca0589401651323
yrsegal <yrsegal@gmail.com>
2017-09-02 09:03:38 -0400

    recipe

--- src/main/java/wiresegal/classy/hats/common/core/CommonProxy.kt
+++ src/main/java/wiresegal/classy/hats/common/core/CommonProxy.kt
@@ -2,12 +2,16 @@ package wiresegal.classy.hats.common.core
 
 import com.teamwizardry.librarianlib.features.base.ModCreativeTab
 import com.teamwizardry.librarianlib.features.helpers.nonnullListOf
+import net.minecraft.init.Blocks
 import net.minecraft.item.ItemStack
 import net.minecraftforge.common.capabilities.CapabilityManager
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.network.NetworkRegistry
+import net.minecraftforge.fml.common.registry.GameRegistry
+import net.minecraftforge.oredict.OreDictionary
+import net.minecraftforge.oredict.ShapedOreRecipe
 import wiresegal.classy.hats.ClassyHats
 import wiresegal.classy.hats.common.gui.HatsGuiFactory
 import wiresegal.classy.hats.common.hat.BaseHatStorage
@@ -45,6 +49,14 @@ open class CommonProxy {
 
     open fun init(e: FMLInitializationEvent) {
         NetworkRegistry.INSTANCE.registerGuiHandler(ClassyHats.INSTANCE, HatsGuiFactory)
+
+        GameRegistry.addRecipe(ShapedOreRecipe(ItemPhantomThread,
+                "LW ",
+                "WSW",
+                " WL",
+                'L', "gemLapis",
+                'W', ItemStack(Blocks.WOOL, 1, OreDictionary.WILDCARD_VALUE),
+                'S', "stickWood"))
     }
 
     open fun post(e: FMLPostInitializationEvent) {
