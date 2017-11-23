c3c54fdd82cf242b3bf8b8bd3760facba960b6b7
InsomniaKitten <admin@designdevils.co.uk>
2017-11-23 16:50:38 +0000

    Use safe access for setting entity data

--- src/main/java/wiresegal/classy/hats/common/core/PacketEntityHatSync.kt
+++ src/main/java/wiresegal/classy/hats/common/core/PacketEntityHatSync.kt
@@ -4,9 +4,6 @@ import com.teamwizardry.librarianlib.core.LibrarianLib
 import com.teamwizardry.librarianlib.features.autoregister.PacketRegister
 import com.teamwizardry.librarianlib.features.network.PacketBase
 import com.teamwizardry.librarianlib.features.saving.Save
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.item.ItemStack
-import net.minecraft.nbt.NBTTagCompound
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext
 import net.minecraftforge.fml.relauncher.Side
 
@@ -18,7 +15,6 @@ import net.minecraftforge.fml.relauncher.Side
 class PacketEntityHatSync(@Save var entity: Int = -1, @Save var name: String = "") : PacketBase() {
     override fun handle(ctx: MessageContext) {
         val entity = LibrarianLib.PROXY.getClientPlayer().world.getEntityByID(entity)
-        if (entity != null)
-            entity.entityData.setString(AttachmentHandler.customKey, name)
+        entity?.entityData?.setString(AttachmentHandler.customKey, name)
     }
 }
